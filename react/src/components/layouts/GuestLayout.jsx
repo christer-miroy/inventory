b6c0b0457e9fad421975f82d3d5bece87152b1f6
Christer Miroy <chrstrzm.mry@gmail.com>
2023-12-23 15:21:33 +0800

    protected route

--- react/src/components/layouts/GuestLayout.jsx
+++ react/src/components/layouts/GuestLayout.jsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const GuestLayout = () => {
+    const {token} =useStateContext();
+
+    if (token) {
+        return <Navigate to="/dashboard" />
+    }
+
   return (
     <div>
         <Outlet />
